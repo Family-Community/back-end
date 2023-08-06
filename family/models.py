5c986ffe6916e8555c424bc5287040e7ea1b92bd
ShinSub98 <wjdtjq26@naver.com>
2023-08-06 16:12:20 +0900

    migrate

--- family/models.py
+++ family/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Group(models.Model):
     family_name = models.CharField(max_length=10)
-    color = models.CharField(max_length=4)
-    entry_number = models.CharField(max_length=10)
+    color = models.CharField(max_length=7)
+    entry_number = models.CharField(max_length=4)
 
 class Member(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
