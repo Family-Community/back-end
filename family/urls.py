f7b66db112c1bbe04ee7cc9626f0a167dd3d088a
ShinSub98 <wjdtjq26@naver.com>
2023-08-06 18:49:14 +0900

    url 추가

--- family/urls.py
+++ family/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views_member
+from . import views_group
 
 app_name = 'family'
 
 urlpatterns = [
+    path('createfamily/', views_group.create_group),
+    path('{:family_code}/profile/create/')
 ]
