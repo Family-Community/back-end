f994983c6408ce6dedf4e9be7b54230cdcebac5b
ShinSub98 <wjdtjq26@naver.com>
2023-08-06 21:23:48 +0900

    .gitignore 추가

--- family/views_member.py
+++ family/views_member.py
@@ -11,8 +11,8 @@ from rest_framework.decorators import api_view
 # Create your views here.
 # 멤버 생성
 @api_view(['POST'])
-def create_member(request, family_code):
-    group = Group.objects.get(family_code = family_code)
+def create_member(request, family_name):
+    group = Group.objects.get(family_name = family_name)
     serializer = MemberSerializer(data = request.data, group = group)
     if serializer.is_valid():
         serializer.save()
@@ -21,8 +21,8 @@ def create_member(request, family_code):
 
 # 멤버 수정
 @api_view(['PUT'])
-def update_member(reqeust, family_code, member_id):
-    member = Member.objects.get(group__family_code = family_code, member_id = member_id)
+def update_member(reqeust, family_name, member_id):
+    member = Member.objects.get(group__family_name = family_name, member_id = member_id)
     serializer = MemberSerializer(member, data = reqeust.data)
     if serializer.is_valid():
         return Response(status=status.HTTP_200_OK)
@@ -30,8 +30,8 @@ def update_member(reqeust, family_code, member_id):
 
 # 멤버 삭제
 @api_view(['DELETE'])
-def delete_member(request, family_code, member_id):
-    member = Member.objects.get(group__family_code = family_code, member_id = member_id)
+def delete_member(request, family_name, member_id):
+    member = Member.objects.get(group__family_name = family_name, member_id = member_id)
     member.delete()
     return Response(status=status.HTTP_200_OK)
 
