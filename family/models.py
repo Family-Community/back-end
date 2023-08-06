5e097b486377455e2c81cdb41667ddadc278df7b
ShinSub98 <wjdtjq26@naver.com>
2023-08-06 16:00:43 +0900

    마이그레이트

--- family/models.py
+++ family/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Group(models.Model):
     family_name = models.CharField(max_length=10)
-    color = models.CharField(max_length=10)
+    color = models.CharField(max_length=4)
     entry_number = models.CharField(max_length=10)
 
 class Member(models.Model):
