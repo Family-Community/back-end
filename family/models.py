77e1de53ef6d988b39cdf903b44940c51cc8c7da
ShinSub98 <wjdtjq26@naver.com>
2023-08-07 16:54:48 +0900

    family_name unique 해제

--- family/models.py
+++ family/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Group(models.Model):
-    family_name = models.CharField(max_length=10, unique=True)
+    family_name = models.CharField(max_length=10)
     color = models.CharField(max_length=7)
     entry_number = models.CharField(max_length=4)
 
