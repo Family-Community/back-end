0d35d95869d9e4061b2cf5e8e441cbc682d2b371
SienaaPark <qkrtldus11@gmail.com>
2023-08-08 19:34:43 +0900

    v.1.1.6

--- contents/urls.py
+++ contents/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from .views import *
 
 app_name = 'contents'
 
 urlpatterns = [
-    path('<int:pk>/<int:member_id>/create/', views.CreateAPIView.create),
-    path('<int:pk>/<int:member_id>/<int:post_id>/delete/',views.DestroyAPIView.destroy),
+    path('<int:pk>/<int:member_id>/create/', CreateContentAPIView.as_view()),
+    path('<int:pk>/<int:member_id>/<int:post_id>/delete/',DeleteContentAPIView.as_view()),
 ]
 
 
