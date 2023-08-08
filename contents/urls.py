7bdeae6813baa808f3ac5a46abb8f03e310bf234
ShinSub98 <wjdtjq26@naver.com>
2023-08-08 16:29:12 +0900

    contents url 주석처리

--- contents/urls.py
+++ contents/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'contents'
 
 urlpatterns = [
-    path('<int:pk>/contents/{int:memver_id}/create/',views.upload_content,name='creat_content'),
+    # path('<int:pk>/contents/{int:memver_id}/create/',views.upload_content,name='creat_content'),
     # path('<int:pk>/contents/{int:memver_id}/{int:post_id}/update/', 업데이트뷰),
     # path('<int:pk>/contents/{int:memver_id}/{int:post_id}/delete/',삭제뷰),
     # path('<int:pk>/contents/{int:memver_id}/{int:post_id}/reaction/',리액션뷰)
