d59d96332eb52d2ba4156bdf6b548ca2e9b84d1e
ShinSub98 <wjdtjq26@naver.com>
2023-08-09 18:22:22 +0900

    cors 수정

--- anbang/settings.py
+++ anbang/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'anbang.urls'
@@ -158,3 +158,4 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:9000",]
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
