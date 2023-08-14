65f4b6e7bd06425d87a1e9201b84d92276e0eb94
ShinSub98 <wjdtjq26@naver.com>
2023-08-14 15:36:21 +0900

    시리얼라이저 주석 추가

--- contents/serializers.py
+++ contents/serializers.py
@@ -2,20 +2,10 @@ from rest_framework import serializers
 from .models import *
 from family.serializers import *
 
-# class ReactionSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Reaction
-#         fields = '__all__'
 
-
-# class ReactionUserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Reaction
-#         fields = ['user_smile', 'user_good', 'user_sad', 'user_heart', 'user_worry', 'user_check']
-
-
+# 개발자 확인용 시리얼라이저
+# 게시글을 작성한 유저에 대한 정보는 없음
 class ContentSerializer(serializers.ModelSerializer):
-    # reaction = ReactionUserSerializer()
     photo = serializers.SerializerMethodField()
     
     def get_image(self, obj):
@@ -27,7 +17,19 @@ class ContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Content
         fields = '__all__'
-        # fields = ['id', 'title', 'content', 'photo', 'date', 'user_smile', 'user_good', 'user_sad', 'user_heart', 'user_worry', 'user_check'] # 'reaction'
+        # fields = [
+            # 'id',
+            # 'title',
+            # 'content',
+            # 'photo',
+            # 'date',
+            # 'user_smile',
+            # 'user_good',
+            # 'user_sad',
+            # 'user_heart',
+            # 'user_worry',
+            # 'user_check'
+        # ]
 
     def get_photo(self, obj):
         if obj.photo:
@@ -36,16 +38,14 @@ class ContentSerializer(serializers.ModelSerializer):
         return None
 
 
+# 유저에게 보여지는 데이터의 시리얼라이저
 class ContentUserSerializer(serializers.ModelSerializer):
     member = MemberWithIDSerializer()
     photo = serializers.SerializerMethodField()
-    # reaction = ReactionUserSerializer()
     
     class Meta:
         model = Content
         fields = '__all__'
-        # fields = ['member', 'title', 'content', 'photo', 'date', 'user_smile', 'user_good', 'user_sad', 'user_heart', 'user_worry', 'user_check'] # 'reaction'
-
 
     def get_photo(self, obj):
         if obj.photo:
@@ -54,6 +54,7 @@ class ContentUserSerializer(serializers.ModelSerializer):
         return None
 
 
+# 게시글 작성 시의 시리얼라이저
 class CreateContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Content
--- family/serializers.py
+++ family/serializers.py
@@ -1,25 +1,28 @@
 from rest_framework import serializers
 from .models import Group, Member
 
-
+# 개발자 확인용 시리얼라이저
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
 
 
+# 그룹의 컬러값을 리턴하기 위한 시리얼라이저
 class GroupColorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ['color']
 
 
+# family_code -> group_pk를 위한 시리얼라이저
 class GroupPkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ['id', 'family_name', 'color']
 
 
+# 개발자 확인용 시리얼라이저
 class MemberSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
     image_original = serializers.SerializerMethodField()
@@ -45,6 +48,8 @@ class MemberSerializer(serializers.ModelSerializer):
     #     member_id = self.context.get('member_id')
     #     return Member.objects.create(group = group, member_id = member_id, **data)
 
+
+# 멤버 생성 시리얼라이저
 class MemberCreateSerializer(serializers.ModelSerializer):
     image_original = serializers.ImageField()
     class Meta:
@@ -95,6 +100,8 @@ class MemberWithIDSerializer(serializers.ModelSerializer):
             return request.build_absolute_uri(obj.image.url)
         return None
 
+
+# 멤버 프로필 이미지 원본 시리얼라이저
 class MemberImageSerializer(serializers.ModelSerializer):
     image_original = serializers.SerializerMethodField()
 
