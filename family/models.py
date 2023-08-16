800c886c224c3e3953be4382b1d0485205dc605f
ShinSub98 <wjdtjq26@naver.com>
2023-08-16 15:45:32 +0900

    원본 이미지 api 작성 중

--- family/models.py
+++ family/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 import uuid, hashlib
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 def generate_hash():
     id = str(uuid.uuid4())
@@ -21,4 +24,34 @@ class Group(models.Model):
 class Member(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
+    image_original = models.ImageField(upload_to="profile_original", null=True, blank=True)
     image = models.ImageField(upload_to="profile", null=True, blank=True)
+
+
+    def save(self, *args, **kwargs):
+        if self.image_original:
+            original_image = Image.open(self.image_original)
+        
+            max_size = (300, 300)
+            
+            width, height = original_image.size
+            if width > height:
+                diff = (width - height) // 2
+                crop_box = (diff, 0, width - diff, height)
+            else:
+                diff = (height - width) // 2
+                crop_box = (0, diff, width, height - diff)
+            
+            cropped_image = original_image.crop(crop_box)
+            cropped_image.thumbnail(max_size, Image.LANCZOS)
+            
+            image_thumb_io = BytesIO()
+            cropped_image.save(image_thumb_io, format='JPEG')  
+            image_thumb = SimpleUploadedFile(
+                name=f'{self.image_original.name.split(".")[0]}_thumb.jpg',  
+                content=image_thumb_io.getvalue(),
+                content_type='image/jpeg' 
+            )
+            self.image.save(image_thumb.name, image_thumb, save=False)
+    
+        super().save(*args, **kwargs)
