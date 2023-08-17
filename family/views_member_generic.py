b23176e7218ced6a6e1faa99705b8563713f5748
Mekdlsdl <ekdlsdl0@hufs.ac.kr>
2023-08-17 12:58:20 +0900

    image null 허용

--- family/views_member_generic.py
+++ family/views_member_generic.py
@@ -15,11 +15,30 @@ from rest_framework.generics import CreateAPIView, DestroyAPIView, UpdateAPIView
 class CreateMember(CreateAPIView):
     serializer_class = MemberCreateSerializer
 
+    def form_valid(self, form):
+        # 파일 필드 업로드 처리
+        if 'image_original' in self.request.FILES:
+            form.instance.image_original = self.request.FILES['image_original']
+        
+        # name 필드 처리
+        name = self.request.POST.get('name', '')
+        form.instance.name = name
+        
+        # ... (다른 필드들 처리)
+        
+        # 데이터가 없을 경우 해당 필드를 비움 (빈 문자열 또는 null로 설정)
+        if 'image_original' not in self.request.FILES:
+            form.instance.image_original = None
+        
+        return super().form_valid(form)
+        
     def perform_create(self, serializer):
         group_pk = self.kwargs['group_pk']
         group = Group.objects.get(pk = group_pk)
         serializer.save(group = group)
         return Response(status=status.HTTP_201_CREATED)
+
+    
 
 # 멤버 삭제
 class DeleteMember(DestroyAPIView):
